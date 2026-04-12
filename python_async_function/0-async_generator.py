7230cee48e530d025e91eeca9ec0c143a42655a4
Qurban <qurbanhuseynzade2007@gmail.com>
2026-04-12 10:44:18 +0000

    Add async_generator coroutine yielding random floats

--- python_async_function/0-async_generator.py
+++ python_async_function/0-async_generator.py
@@ -6,8 +6,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Coroutine that loops 10 times, each time waits 1 second,
-    then yields a random number between 0 and 10."""
-    for _ in range(10):
+    """Yield 10 random floats."""
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
