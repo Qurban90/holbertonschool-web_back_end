7598bfef5aae390061e5a210c5eb0ee62879d75d
qurban <qurbanhuseynzade@007@gmail.com>
2026-04-12 17:00:45 +0000

    Fix async_generator formatting

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-"""Async generator module."""
+"""Async Generator module"""
+
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Coroutine that loops 10 times, each time asynchronously
-    waits 1 second, then yields a random number between 0 and 10.
-    """
+    """Loop 10 times, wait 1 second, yield random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
